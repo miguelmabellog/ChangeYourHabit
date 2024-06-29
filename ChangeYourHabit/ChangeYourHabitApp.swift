67d9b7fd63753f6d063e45d4eeae55a7b67c54dd
miguel <belloingmiguel@gmail.com>
2024-06-29 17:53:52 -0500

    Initial Commit

---
+++ ChangeYourHabit/ChangeYourHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeYourHabitApp.swift
+//  ChangeYourHabit
+//
+//  Created by miguel angel bello garcia on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChangeYourHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
